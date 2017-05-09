a90c64a28dfd0299aec7f60ff8b02114c4601e90
Abel van Beek <a.vanbeek@students.uu.nl>
2017-05-09 16:22:25 +0200

    Added useful classes.

---
+++ Camera.cs
@@ -0,0 +1,39 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace template
+{
+    class Camera
+    {
+        Vector3 position;
+        Vector3 direction;
+        Vector3[] screenCorners;
+        Vector3[] planeCorners;
+        float screenDistance;
+
+        public Camera()
+        {
+            position = new Vector3(0,0,0);
+            direction = new Vector3(0, 0, 1);
+            screenDistance = 10;
+
+            //screen
+            screenCorners[0] = new Vector3(0, 0, 400);
+            screenCorners[0] = new Vector3(512, 0, 400);
+            screenCorners[0] = new Vector3(512, 512, 400);
+            screenCorners[0] = new Vector3(0, 512, 400);
+
+            //plane
+            planeCorners[0] = new Vector3(0, 0, -1);
+            planeCorners[0] = new Vector3(100, 0, -1);
+            planeCorners[0] = new Vector3(100, 100, -1);
+            planeCorners[0] = new Vector3(0, 100, -1);
+
+
+        }
+    }
+}
---
+++ Light.cs
@@ -0,0 +1,19 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace template
+{
+    class Light
+    {
+        Vector3 Origin;
+        Vector3 intensity;
+        public Light()
+        {
+            intensity = new Vector3(1f, 1f, 1f);
+        }
+    }
+}
