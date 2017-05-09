636f6d72322c22310597f1379cee977ccd593c43
Abel van Beek <a.vanbeek@students.uu.nl>
2017-05-09 14:28:22 +0200

    Initializing Git Repository.

---
+++ game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Template
+{
+    class Game
+    {
+
+	    //Member Variables_ Place member variables here
+	    public Surface screen;
+
+	    //Init_ Initializes the program
+	    public void Init()
+	    {        
+	    }
+
+	    //Tick_ Renders one frame
+	    public void Tick()
+	    {
+
+		    screen.Clear( 0 );
+		    screen.Print( "Hello World!", 2, 2, 0xffffff );
+            screen.Line(2, 20, 160, 20, 0xff0000);
+	    }
+    }
+
+}
